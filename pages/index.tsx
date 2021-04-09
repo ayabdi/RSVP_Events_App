887e42ea7f6148ec58e0c84fcb0573f1f2fc8fb6
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-04-09 16:20:18 +0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,22 @@
 import Link from 'next/link'
-import { useState } from 'react'
-import {
-  useViewerQuery,
-  useUpdateNameMutation,
-  ViewerDocument,
-} from '../lib/viewer.graphql'
-import { initializeApollo } from '../lib/apollo'
+
+import {useQuery} from '@apollo/client'
+
+import {getAllEvents} from './controllers/EventController'
+
+import {initializeApollo, ResolverContext, getHeaders} from '../lib/apollo'
+
+
 
 const Index = () => {
-  const { viewer } = useViewerQuery().data!
-  const [newName, setNewName] = useState('')
-  const [updateNameMutation] = useUpdateNameMutation()
 
-  const onChangeName = () => {
-    updateNameMutation({
-      variables: {
-        name: newName,
-      },
-      //Follow apollo suggestion to update cache
-      //https://www.apollographql.com/docs/angular/features/cache-updates/#update
-      update: (
-        store,
-        {
-          data: {
-            updateName: { name },
-          },
-        }
-      ) => {
-        // Read the data from our cache for this query.
-        const { viewer } = store.readQuery({ query: ViewerDocument })
-        const newViewer = { ...viewer }
-        // Add our comment from the mutation to the end.
-        newViewer.name = name
-        // Write our data back to the cache.
-        store.writeQuery({ query: ViewerDocument, data: { viewer: newViewer } })
-      },
-    })
-  }
+  const {loading, data} = useQuery(getAllEvents())
+  console.log(data)
+ 
 
   return (
     <div>
-      You're signed in as {viewer.name} and you're {viewer.status}. Go to the{' '}
+      You're signed in as  and you're  Go to the{' '} Age 
       <Link href="/about">
         <a>about</a>
       </Link>{' '}
@@ -49,26 +25,24 @@ const Index = () => {
         <input
           type="text"
           placeholder="your new name..."
-          onChange={(e) => setNewName(e.target.value)}
+          // onChange={(e) => setNewName(e.target.value)}
         />
-        <input type="button" value="change" onClick={onChangeName} />
+        <input type="button" value="change"  />
       </div>
     </div>
   )
 }
-
-export async function getStaticProps() {
-  const apolloClient = initializeApollo()
-
-  await apolloClient.query({
-    query: ViewerDocument,
-  })
-
+export async function getServerSideProps(context:any){
+  const headers = getHeaders(context)
+  const apolloClient = await initializeApollo(headers)
   return {
     props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
+    initialApolloState: apolloClient.cache.extract(),
+
+    }
   }
 }
+
+
 
 export default Index

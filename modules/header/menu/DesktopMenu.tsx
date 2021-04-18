d8dab1a54081ca9ff1e1a603c6c773fc13ccaea1
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-04-19 00:14:48 +0300

    navigation bar with menu desktop/mobile

---
+++ modules/header/menu/DesktopMenu.tsx
@@ -0,0 +1,103 @@
+import { FC, Fragment } from "react";
+import { session, signIn, signOut } from "next-auth/client";
+import { Menu, Transition } from "@headlessui/react";
+import { Session } from "next-auth";
+
+interface DesktopMenuProps {
+  profile: String[];
+  isWhite: Boolean;
+  session?: Session | null | undefined;
+  loading?: Boolean;
+}
+const DesktopMenu: FC<DesktopMenuProps> = (props ): JSX.Element => {
+  const classNames = (...classes: String[]) => {
+    return classes.filter(Boolean).join(" ");
+  };
+
+  return (
+    <div className="hidden md:block">
+      <div className="ml-4 flex items-center md:ml-6">
+        {/* Profile dropdown */}
+
+        {!props.session ? (
+          <a
+            href={`/api/auth/signin`}
+            className="font-medium text-indigo-600 hover:text-indigo-500"
+            onClick={(e) => {
+              e.preventDefault();
+              signIn();
+            }}
+          >
+            Log in
+          </a>
+        ) : (
+          <Menu as="div" className="ml-3 relative">
+            {({ open }) => (
+              <>
+                <div>
+                  <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  show={open}
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items
+                    static
+                    className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    {props.profile.map((item, itemIdx) => (
+                      <Menu.Item key={itemIdx}>
+                        {({ active }) =>
+                          item == "Sign out" ? (
+                            <a
+                              href={`/api/auth/signout`}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                              onClick={(e) => {
+                                e.preventDefault();
+                                signOut();
+                              }}
+                            >
+                              {item}
+                            </a>
+                          ) : (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              {item}
+                            </a>
+                          )
+                        }
+                      </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                </Transition>
+              </>
+            )}
+          </Menu>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DesktopMenu;
---
+++ modules/header/navbar.tsx
@@ -0,0 +1,89 @@
+import React, { FC } from "react";
+import { Fragment } from "react";
+import { Disclosure } from "@headlessui/react";
+import DesktopMenu from "./menu/DesktopMenu";
+import MobileMenu from "./menu/MobileMenu";
+import { MenuPanel } from "./menu/MenuPanel";
+import { useSession } from "next-auth/client";
+
+export type NavType = {
+  title: String;
+  href: string;
+}[];
+
+interface NavProps {
+  isWhite: Boolean;
+  navigation: NavType;
+}
+
+const Navbar: FC<NavProps> = (props): JSX.Element => {
+  const [session , isloading] = useSession();
+  const profile = ["Your Profile", "Settings", "Sign out"];
+
+  return (
+    <Disclosure
+      as="nav"
+      className={props.isWhite ? "bg-white border-b-2" : "bg-gray-800"}
+    >
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-20">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-8 w-8"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    {props.navigation.map((item, itemIdx) =>
+                      itemIdx === 0 ? (
+                        <Fragment key={itemIdx}>
+                          {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                          <a
+                            href={item.href}
+                            className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                          >
+                            {item.title}
+                          </a>
+                        </Fragment>
+                      ) : (
+                        <a
+                          key={itemIdx}
+                          href={item.href}
+                          className={` hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${
+                            props.isWhite ? "text-black" : "text-gray-300"
+                          }`}
+                        >
+                          {item.title}
+                        </a>
+                      )
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <DesktopMenu
+                profile={profile}
+                isWhite={props.isWhite}
+                session={session}
+                loading = {isloading}
+              />
+              <MobileMenu open={open} />
+            </div>
+          </div>
+          <MenuPanel
+            navigation={props.navigation}
+            profile={profile}
+            isWhite={props.isWhite}
+          />
+        </>
+      )}
+    </Disclosure>
+  );
+};
+
+export default Navbar;

464b15d2ed1523b4f2c16facb0fbf7349e0dcdd6
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-04-22 19:12:21 +0300

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 
 const colors = require('tailwindcss/colors')
 module.exports = {
+  
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      green: colors.green
-    }
+    
   },
   variants: {
     extend: {},

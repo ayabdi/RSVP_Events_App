1bac100c342960eeb0c85bc9a41215d093309dd6
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-04-22 19:15:02 +0300

    form hooks

---
+++ modules/dashboard/useForm.tsx
@@ -0,0 +1,66 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { useSession } from "next-auth/client";
+import { useState, ChangeEvent, FormEvent } from "react";
+import { EventsQueryType, insertEvent } from "../../lib/queries/eventQueries";
+import { getUser } from "../../lib/queries/userQueries";
+
+const useForm = (session :any ) => {
+   
+    const { data} = useQuery(getUser(session && session.user.email));
+    console.log(session)
+  const initialState: EventsQueryType = {
+    event_name: " ",
+    event_date: new Date(),
+    event_type: " ",
+    country: " ",
+    address: " ",
+    zip: " ",
+    state:  " ",
+    city: ' ',
+    host_id: data?.RSVP_Users[0]?.id,
+    name: session.user.name,
+    email: session.user.email
+  };
+
+  const [eventData, setEventData] = useState<EventsQueryType>(initialState);
+  console.log(eventData)
+  const [mutate] = useMutation(insertEvent());
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setEventData({
+      ...eventData,
+      [name]: value,
+    });
+  };
+  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setEventData({
+      ...eventData,
+      [name]: value,
+    });
+  };
+
+  const [submitted, setSubmitted] = useState(false)
+  const onSubmit = async (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmitted(false)
+    try {
+      mutate({
+        variables: eventData,
+      });
+      setSubmitted(true)
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  };
+  return {
+      handleChange,
+      handleSelectChange,
+      onSubmit,
+      eventData,
+      setEventData, submitted
+  };
+};
+
+export default useForm;

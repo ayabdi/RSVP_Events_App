010ebad95d8086899bce7dc31ad4e78f292f3ed4
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-04-28 01:23:12 +0300

    invite response functionality

---
+++ pages/invite/accept/[token].tsx
@@ -0,0 +1,10 @@
+import StatusResponse from "../../../modules/shared_components/StatusResponse";
+
+const Invite = () => {
+  return (
+    <StatusResponse response = 'attending' message = "Thank you for accepting the invite"/>
+  )
+ 
+};
+
+export default Invite;
---
+++ pages/invite/reject/[token].tsx
@@ -0,0 +1,10 @@
+import StatusResponse from "../../../modules/shared_components/StatusResponse";
+
+const Invite = () => {
+  return (
+    <StatusResponse response = 'not attending' message = "Thank you for your response. Sorry you wont be able to make it."/>
+  )
+ 
+};
+
+export default Invite;

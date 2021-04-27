4835336c3cf0df75e820b50033c081f623242383
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-04-28 01:22:44 +0300

    invite response page component

---
+++ modules/shared_components/StatusResponse.tsx
@@ -0,0 +1,49 @@
+import { useRouter } from "next/dist/client/router";
+import jwt from "jsonwebtoken";
+import { useMutation } from "@apollo/client";
+import { updateInviteStatus } from "../queries/inviteeQueries";
+import { FC} from "react";
+
+interface UpdateInviteStatus {
+  id: Number;
+  status: String;
+}
+
+interface StatsuResponseProps {
+    response : String,
+    message : String
+}
+const StatusResponse : FC<StatsuResponseProps> = (props) => {
+  const router = useRouter();
+  
+  const { token } = router.query;
+  let decodedToken: Object;
+  let updateData: UpdateInviteStatus | undefined = undefined
+ 
+  const [mutate] = useMutation(updateInviteStatus());
+
+
+  if (token) {
+    try {
+      const stringToken: string = token.toString();
+      decodedToken = jwt.verify(stringToken, "shhhht");
+       updateData = {
+        id: decodedToken.invite_id,
+        status: props.response,
+      };
+      console.log(updateData)
+      mutate({
+        variables: updateData,
+      });
+
+      !updateData.id && router.push('../../')
+    } catch (error) {
+      console.log(error + "status update failed");
+      router.push('../../')
+    }
+  }
+  
+  return updateData?.id?(<div>{props.message}</div>) : null
+};
+
+export default StatusResponse;

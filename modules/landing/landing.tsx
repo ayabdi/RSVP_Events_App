5dace0778ef4e917e3d7c9cdbb8c36b507c39f52
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-04-28 01:20:35 +0300

    show dashboard link when signed in

--- modules/landing/landing.tsx
+++ modules/landing/landing.tsx
@@ -1,16 +1,16 @@
 import { useSession } from "next-auth/client";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Navbar from "../header/navbar";
 import { Hero } from "./hero";
 
 export const Landing = () => {
-  const[session] = useSession()
+  const [session, loading] = useSession();
   const navigation = [
     {
       title: "Home",
       href: "#",
     },
-   
+
     {
       title: "About",
       href: "#",
@@ -20,15 +20,19 @@ export const Landing = () => {
       href: "#",
     },
   ];
-  useEffect(() => {
-      navigation.push({title:'Dashboard', href: '/dashboard'})
-      return () => {
-          
-      }
-  }, [session])
+
+  if (session) {
+    navigation.push({ title: "Dashboard", href: "/dashboard" });
+  }
+
   return (
     <>
-      <Navbar isWhite={true} navigation={navigation} />
+      <Navbar
+        isWhite={true}
+        navigation={navigation}
+        session={session}
+        isLoading={loading}
+      />
       <Hero />
     </>
   );
